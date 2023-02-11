c9dbc131403a17ef4bdd6ef6f8e6237bc952229f
Onisueka GTO <oni24hours@hotmail.com>
2023-02-11 15:05:48 +0700

    - pull api and display book list

--- resources/js/components/Books.jsx
+++ resources/js/components/Books.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Books() {
+    const [bookDatas, setBookDatas] = useState([]);
+
+    useEffect(() => {
+
+        async function getBooks() {
+            const result = await axios(
+                'api/books',
+            );
+            setBookDatas(result.data || []);
+        }
+
+        getBooks();
+
+
+    }, []);
+
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
+                    <div className="py-4">
+                        <button className="btn btn-primary">
+                            Create Book
+                        </button>
+                    </div>
                     <div className="card">
                         <div className="card-header">Books</div>
 
@@ -14,18 +35,31 @@ function Books() {
                                 <thead>
                                     <tr>
                                         <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Authour</th>
+                                        <th scope="col">Price</th>
+                                        <th scope="col">Created At</th>
+                                        <th scope="col">Updated At</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
+                                    {bookDatas.map(item => (
+                                        <tr key={item.book_id}>
+                                            <th scope="row">
+                                                <button className="btn btn-secondary">
+                                                    Edit
+                                                </button>
+                                                <button className="btn btn-danger ms-2">
+                                                    Delete
+                                                </button>
+                                            </th>
+                                            <td>{item.title}</td>
+                                            <td>{`${item.author_first_name} ${item.author_last_name}`}</td>
+                                            <td>{item.price}</td>
+                                            <td>{item.created_at}</td>
+                                            <td>{item.updated_at}</td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
